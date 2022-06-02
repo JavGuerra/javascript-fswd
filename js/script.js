7e7ce46bf3c138350c8d88551831e1deb1f6b2ca
JavGuerra <javguerra@badared.com>
2022-06-02 08:14:12 +0200

    funciones

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
 let intervalo = spin = 0;
+let aciertos = 9;
 
 const elDialogo = elemento('#error'  );
 const elMsgErr  = elemento('#msgErr' );
-const btnAcepta = elemento('#aceptar');
 const elZona    = elemento('#zona'   );
 
 const elBienven = elemento('#bienven');
 const elCuestio = elemento('#cuestio');
 const elResulta = elemento('#resulta');
+const elPuntua  = elemento('#puntos' );
 
+const btnJugar  = elemento('#jugar'  );
+const btnFinal  = elemento('#final'  );
+const btnInicio = elemento('#inicio' );
+const btnAcepta = elemento('#aceptar');
+
+btnJugar.onclick  = jugar;
+btnFinal.onclick  = final;
+btnInicio.onclick = inicio;
 btnAcepta.onclick = cierraVentanaModal;
+
 
 
 // abreVentanaModal('Mensaje de ejemplo.');
 
-ponSpin(true);
-setTimeout(() => ponSpin(false), 5000);
+// ponSpin(true);
+// setTimeout(() => ponSpin(false), 5000);
+
+
+
+function jugar() {
+    muestra(elBienven, false);
+    muestra(elCuestio, true);
+}
+
+function final() {
+    muestra(elCuestio, false);
+    muestra(elResulta, true);
+}
+
+function inicio() {
+    html(elPuntua, aciertos, true);
+    muestra(elResulta, false);
+    muestra(elBienven, true);
+}
