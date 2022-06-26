d766c9c23bbc0a1fa407b3b7d908a37de6551548
JavGuerra <javguerra@badared.com>
2022-06-26 02:26:09 +0200

    firebase

--- js/firebase.js
+++ js/firebase.js
@@ -8,164 +8,161 @@ const app  = initFirebase();
 const auth = getAuth(app);
 const db   = getDatabase(app);
 
-let msg = '';
 let play = true;
 
 onAuthStateChanged(auth, (user) => {
+  if (user && play) window.location.href = "./game.html";
+  else firebase();
+});
 
-  if (user && play) {
+function firebase() {
 
-    window.location.href = "./game.html";
+  play = false;
 
-  } else {
-    
-    play = false;
+  const elDialog  = el('#error'  );
+  const elErrMsg  = el('#errMsg' );
+  const btnOK     = el('#ok'     );
+  const elZone    = el('#zone'   );
 
-    const elDialog  = el('#error'  );
-    const elErrMsg  = el('#errMsg' );
-    const btnOK     = el('#ok'     );
-    const elZone    = el('#zone'   );
+  let spins = 0;
+  let areSpins = 0;
+  let msg = '';
 
-    let spins = 0;
-    let areSpins = 0;
+  let mode = false;
+  showEl(el('#data'), false);
+  switchMode();
 
-    let mode = false;
-    showEl(el('#datos'), false);
-    switchMode();
+  el('#sndLogin').onclick = (e) => loginUser(e, auth, db);
+  el('#sndReg'  ).onclick = (e) => signUpUser(e, auth, db);
+  el('#lnkLogin').onclick = switchMode;
+  el('#lnkReg'  ).onclick = switchMode;
 
-    el('#envLogin').onclick = (e) => loginUser(e, auth, db);
-    el('#envAlta' ).onclick = (e) => signUpUser(e, auth, db);
-    el('#lnkLogin').onclick = switchMode;
-    el('#lnkAlta' ).onclick = switchMode;
-
-    function switchMode() {
-      mode = !mode;
-      showEl(el('#login'), mode);
-      showEl(el('#alta'), !mode);
-    }
-
-
-    function loginUser(e, auth, db) {
-      if (document.formLogin.checkValidity()) {
-        e.preventDefault();
-        setInactiveBtn(el('#envLogin'), true);
-        setSpin(true);
-
-        const correo = document.formLogin.correo.value;
-        const passwd = document.formLogin.passwd.value;
-        
-        signInWithEmailAndPassword(auth, correo, passwd)
-          .then(response => {
-
-            console.log('User successfully authenticated.');
-            const userId = response.user.uid;
-
-            // TODO Mostrar datos
-            // getUserData(db, userId);
-            
-            showEl(el('#login'), false);
-            showEl(el('#datos'), true);
-          })
-          .catch(err => {
-            openDialog('User not available.');
-            console.log(err);
-          });
-
-          setSpin(false);
-          setInactiveBtn(el('#envLogin'), false);
-      }
-    }
+  function switchMode() {
+    mode = !mode;
+    showEl(el('#login'), mode);
+    showEl(el('#regtr'), !mode);
+  }
 
 
-    function signUpUser(e, auth, db) {
-      if (document.formAlta.checkValidity()) {
-        e.preventDefault();
-        setInactiveBtn(el('#envAlta'), true);
-        setSpin(true);
-
-        const nombre  = document.formAlta.nombre.value;
-        const edad    = document.formAlta.edad.value;
-        const direcc  = document.formAlta.direccion.value;
-        const correo  = document.formAlta.correo2.value;
-        const passwd  = document.formAlta.passwd1.value;
-        const passwd2 = document.formAlta.passwd2.value;
-
-        if (passwd !== '' && passwd2 !== '' && passwd === passwd2) {
-          if (passwd.length() >= 6) {
-            createUserWithEmailAndPassword(auth, correo, passwd)
-            .then(response => {
-              console.log('User successfully created.');
-              const userId = response.user.uid;
-              const data = {
-              nombre: nombre,
-              edad: edad,
-              direcc: direcc,
-              correo: correo
-              };
-
-              // TODO subir datos
-              // setUserData(db, userId, data);
-
-              switchMode();
-            })
-            .catch(err => {
-              openDialog('It was not possible to register the user.');
-              console.log(err);
-            });
-          } else {
-            msg = 'The password must be at least 6 characters long.';
-            openDialog(msg); console.log(msg);
-          }
-        } else {
-          msg = 'Passwords do not match.';
-          openDialog(msg); console.log(msg);
-        }
-
-        setSpin(false);
-        setInactiveBtn(el('#envLogin'), false);
-      }
-    }
-
-
-    function getUserData(db, userId) {
+  function loginUser(e, auth, db) {
+    if (document.formLogin.checkValidity()) {
+      e.preventDefault();
+      setInactiveBtn(el('#sndLogin'), true);
       setSpin(true);
 
-      get(child(ref(db), `users/${userId}`))
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            console.log('Datos de usuario obtenidos correctamente.');
-            el('#dataNombre').innerHTML = snapshot.val().nombre;
-            el('#datoEdad'  ).innerHTML = snapshot.val().edad;
-            el('#datoDirec' ).innerHTML = snapshot.val().direcc;
-            el('#datoCorreo').innerHTML = snapshot.val().correo;
-          } else {
-            msg = 'No data available.';
-            openDialog(msg); console.log(msg);
-          }
+      const email  = document.formLogin.email.value;
+      const passwd = document.formLogin.passwd.value;
+      
+      signInWithEmailAndPassword(auth, email, passwd)
+        .then(response => {
+
+          console.log('User successfully authenticated.');
+          const userId = response.user.uid;
+
+          // TODO Mostrar datos
+          // getUserData(db, userId);
+          
+          showEl(el('#login'), false);
+          showEl(el('#data' ), true );
         })
         .catch(err => {
-          openDialog('It has not been possible to obtain user data.');
+          openDialog('User not available.');
           console.log(err);
         });
 
       setSpin(false);
+      setInactiveBtn(el('#sndLogin'), false);
     }
-
-
-    function setUserData(db, userId, data) {
-      setSpin(true);
-
-      try {
-        set(ref(db, `users/${userId}`), data);
-      }
-      catch (err) {
-        openDialog('It has not been possible to add user data.');
-        console.log(err);
-      };
-
-      setSpin(false);
-    }
-
   }
 
-});
+
+  function signUpUser(e, auth, db) {
+    if (document.formAlta.checkValidity()) {
+      e.preventDefault();
+      setInactiveBtn(el('#sndReg'), true);
+      setSpin(true);
+
+      const name    = document.formAlta.name.value;
+      const age     = document.formAlta.age.value;
+      const adress  = document.formAlta.adress.value;
+      const email   = document.formAlta.email2.value;
+      const passwd  = document.formAlta.passwd1.value;
+      const passwd2 = document.formAlta.passwd2.value;
+
+      if (passwd !== '' && passwd2 !== '' && passwd === passwd2) {
+        if (passwd.length() >= 6) {
+          createUserWithEmailAndPassword(auth, email, passwd)
+          .then(response => {
+            console.log('User successfully created.');
+            const userId = response.user.uid;
+            const data = {
+            name: name,
+            age: age,
+            adress: adress,
+            email: email
+            };
+
+            // TODO subir datos
+            // setUserData(db, userId, data);
+
+            switchMode();
+          })
+          .catch(err => {
+            openDialog('It was not possible to register the user.');
+            console.log(err);
+          });
+        } else {
+          msg = 'The password must be at least 6 characters long.';
+          openDialog(msg); console.log(msg);
+        }
+      } else {
+        msg = 'Passwords do not match.';
+        openDialog(msg); console.log(msg);
+      }
+
+      setSpin(false);
+      setInactiveBtn(el('#sndLogin'), false);
+    }
+  }
+
+
+  function getUserData(db, userId) {
+    setSpin(true);
+
+    get(child(ref(db), `users/${userId}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log('User data obtained correctly.');
+          el('#dataName'  ).innerHTML = snapshot.val().name;
+          el('#dataAge'   ).innerHTML = snapshot.val().age;
+          el('#dataAdress').innerHTML = snapshot.val().adress;
+          el('#dataEmail' ).innerHTML = snapshot.val().email;
+        } else {
+          msg = 'No data available.';
+          openDialog(msg); console.log(msg);
+        }
+      })
+      .catch(err => {
+        openDialog('It has not been possible to obtain user data.');
+        console.log(err);
+      });
+
+    setSpin(false);
+  }
+
+
+  function setUserData(db, userId, data) {
+    setSpin(true);
+
+    try {
+      set(ref(db, `users/${userId}`), data);
+    }
+    catch (err) {
+      openDialog('It has not been possible to add user data.');
+      console.log(err);
+    };
+
+    setSpin(false);
+  }
+
+}
--- js/script.js
+++ js/script.js
@@ -2,229 +2,226 @@ import { getQuestionFireBase } from './getQuestion.js';
 import { logoutUser, auth, onAuthStateChanged } from './logoutUser.js';
 
 onAuthStateChanged(auth, (user) => {
+    if (!user) window.location.href = "./index.html";
+    else game();  
+});
 
-    if (!user) {
+function game() {
 
-        window.location.href = "./index.html";
+    let hits   = 0;     // Hist in a quiz
+    let qIndex = 1;     // Actual Q&A order number
+    let numQ   = 10;    // Number of total questions per quiz
+    let questions = []; // The quiz questions
 
-    } else {
+    const form = document.quizForm;
 
-        let hits   = 0;     // Hist in a quiz
-        let qIndex = 1;     // Actual Q&A order number
-        let numQ   = 10;    // Number of total questions per quiz
-        let questions = []; // The quiz questions
+    const elWelcome = el('#welcome');
+    const elTable   = el('#table'  );
+    const elTmeter  = el('#tmeter' );
+    const elQuiz    = el('#quiz'   );
+    const elResult  = el('#result' );
+    const elHits    = el('#hits'   );
+    const elCatego  = el('#catego' );
+    const elDiffic  = el('#diffic' );
+    const elQuesti  = el('#questi' );
+    const elNothing = el('#nothing');
 
-        const form = document.quizForm;
+    const btnOpt1   = el('#opt1');
+    const btnOpt2   = el('#opt2');
+    const btnOpt3   = el('#opt3');
+    const btnOpt4   = el('#opt4');
 
-        const elWelcome = el('#welcome');
-        const elTable   = el('#table'  );
-        const elTmeter  = el('#tmeter' );
-        const elQuiz    = el('#quiz'   );
-        const elResult  = el('#result' );
-        const elHits    = el('#hits'   );
-        const elCatego  = el('#catego' );
-        const elDiffic  = el('#diffic' );
-        const elQuesti  = el('#questi' );
-        const elNothing = el('#nothing');
+    btnOpt1.onclick = () => setInactiveBtn(btnSend, false);
+    btnOpt2.onclick = () => setInactiveBtn(btnSend, false);
+    btnOpt3.onclick = () => setInactiveBtn(btnSend, false);
+    btnOpt4.onclick = () => setInactiveBtn(btnSend, false);
 
-        const btnOpt1   = el('#opt1');
-        const btnOpt2   = el('#opt2');
-        const btnOpt3   = el('#opt3');
-        const btnOpt4   = el('#opt4');
+    const btnPlay   = el('#play' );
+    const btnStart  = el('#start');
+    const btnClose  = el('#close');
+    const btnSend   = el('#send' );
 
-        btnOpt1.onclick = () => setInactiveBtn(btnSend, false);
-        btnOpt2.onclick = () => setInactiveBtn(btnSend, false);
-        btnOpt3.onclick = () => setInactiveBtn(btnSend, false);
-        btnOpt4.onclick = () => setInactiveBtn(btnSend, false);
+    const btnExit = el('#sendExit');
 
-        const btnPlay   = el('#play' );
-        const btnStart  = el('#start');
-        const btnClose  = el('#close');
-        const btnSend   = el('#send' );
+    btnPlay.onclick  = play;
+    btnStart.onclick = start;
+    btnClose.onclick = start;
+    btnSend.onclick  = event => checkAnswer(event);
 
-        const btnExit = el('#sendExit');
+    btnExit.onclick = (event) => logoutUser(event, auth);
 
-        btnPlay.onclick  = play;
-        btnStart.onclick = start;
-        btnClose.onclick = start;
-        btnSend.onclick  = event => checkAnswer(event);
-
-        btnExit.onclick = (event) => logoutUser(event, auth);
-
-        setInactiveBtn(btnPlay, true);
-        hideScores();
-        initialise( () => { if(localStorage.scores) showScores() } );
-        getQuiz();
+    setInactiveBtn(btnPlay, true);
+    hideScores();
+    initialise( () => { if(localStorage.scores) showScores() } );
+    getQuiz();
 
 
-        /**
-         * Start the quiz
-         */
-        function play() {
-            window.scroll(0, 0);
-            setInactiveBtn(btnSend, true);
-            showEl(elWelcome, false);
-            showEl(elQuiz, true);
-            askQuestion();
-        }
-
-        /**
-         * Start the endgame actions
-         */
-        function end() {
-            setHand(hits);
-            elHits.innerHTML = hits;
-            showEl(elQuiz,  false);
-            showEl(elResult, true);
-            saveScore();
-        }
-
-        /**
-         * Start the game again
-         */
-        function start() {
-            window.scroll(0, 0);
-            setInactiveBtn(btnPlay, true);
-            hideScores();
-            showEl(elQuiz,   false);
-            showEl(elResult, false);
-            showEl(elWelcome, true);
-            if (localStorage.scores) showScores();
-            hits   = 0;
-            qIndex = 1;
-            getQuiz();
-        }
-
-        /**
-         * Displays the scores stored in localStorage
-         */
-        function showScores() {
-            let tbody, tr;
-            let value = 0;
-            let avail = 0;
-            let range = 10;    // Maximum score per quiz
-            let numScores = 5; // Number of scores to be displayed  
-            let yChart = [];   // List of chart ordinates
-
-            setSpin(true);
-            setChartLine();
-            showEl(elNothing, false);
-            showEl(elTmeter, true);
-            showEl(chart, true);
-
-            let scores = JSON.parse(localStorage.scores);
-
-            tbody = createTable(elTable, 'scoresTable', ['#', 'Hits', 'Date & Time']);
-            scores.slice(-numScores).forEach((score, i) => {
-                tr = createEl(tbody, 'tr');
-                createEl(tr, 'td', i + 1);
-                createEl(tr, 'th', score.hits.toString()); // '0'
-                createEl(tr, 'td', score.dateTime);
-                yChart.push(score.hits);
-                value += score.hits;
-                avail++;
-            })
-
-            setChartLine(yChart);
-            setMeter(value, range, numScores, avail);
-
-            setSpin(false);
-        }
-
-        /**
-         * Hide the scoring area
-         */
-        function hideScores() {
-            elTable.innerHTML = '';
-            showEl(elTmeter, false);
-            showEl(chart, false);
-        }
-
-        /**
-         * Saves the score of the completed quiz
-         */
-        function saveScore() {
-            let scores = [];
-            setSpin(true);
-            setInactiveBtn(btnStart, true);
-
-            if(localStorage.scores) scores = JSON.parse(localStorage.scores);
-            
-            scores.push({'hits': hits, 'dateTime': currentDateTime()});
-            localStorage.setItem('scores', JSON.stringify(scores.slice(-100))); 
-            
-            setSpin(false);
-            setInactiveBtn(btnStart, false);
-        }
-
-        /**
-         * Gets the questions for the Quiz
-         */
-        function getQuiz() {
-            // We ask for the numQ minus one, which we will get apart from the JSON
-            let address  = `https://opentdb.com/api.php?amount=${numQ - 1}&type=multiple`;
-
-            // Gets the questions (minus one) from the API
-            let query = async data => {
-                if (data.response_code) throw Error('API #' + data.response_code);
-                questions = data.results;
-                let question = await getQuestionFireBase(); // Get one question from Firebase
-                questions.unshift(question);
-                setInactiveBtn(btnPlay, false);
-            };
-
-            fetchAPI(address, query);
-        }
-
-        /**
-         * Show a new question
-         */
-        function askQuestion() {
-            let cards = fisherYatesShuffle([0, 1, 2, 3]);
-            let question = questions[qIndex - 1];
-
-            elCatego.innerHTML = question.category;
-            elDiffic.innerHTML = question.difficulty;
-            elQuesti.innerHTML = question.question;
-
-            cards.forEach((card, i) => {
-                el(`#opt${i + 1}`).checked = false;
-                el(`#opt${i + 1}`).setAttribute('value', card); 
-                if (card) {
-                    el(`label[for=opt${i + 1}]`).innerHTML = question.incorrect_answers[card - 1];
-                } else {
-                    el(`label[for=opt${i + 1}]`).innerHTML = question.correct_answer;
-                    console.log(decodeHTMLEntities(question.correct_answer)); // Did someone ask for help?;
-                }
-            })
-
-            setProgress(numQ, qIndex -1);
-        }
-
-        /**
-         * Check the form
-         * @param {event} event prevent
-         */
-        function checkAnswer(event) {
-            event.preventDefault(); // Firefox needs you!
-            setInactiveBtn(btnSend, true);
-            
-            if (!el('input[name="opt"]:checked')) {
-                openDialog('Select an option');
-            } else {
-                window.scroll(0, 0);
-
-                if (form.opt.value == 0) hits++;
-
-                if (qIndex == numQ) {
-                    end();
-                } else {
-                    qIndex++;
-                    askQuestion();
-                }
-            }
-        }
-        
+    /**
+     * Start the quiz
+     */
+    function play() {
+        window.scroll(0, 0);
+        setInactiveBtn(btnSend, true);
+        showEl(elWelcome, false);
+        showEl(elQuiz, true);
+        askQuestion();
     }
 
-});
+    /**
+     * Start the endgame actions
+     */
+    function end() {
+        setHand(hits);
+        elHits.innerHTML = hits;
+        showEl(elQuiz,  false);
+        showEl(elResult, true);
+        saveScore();
+    }
+
+    /**
+     * Start the game again
+     */
+    function start() {
+        window.scroll(0, 0);
+        setInactiveBtn(btnPlay, true);
+        hideScores();
+        showEl(elQuiz,   false);
+        showEl(elResult, false);
+        showEl(elWelcome, true);
+        if (localStorage.scores) showScores();
+        hits   = 0;
+        qIndex = 1;
+        getQuiz();
+    }
+
+    /**
+     * Displays the scores stored in localStorage
+     */
+    function showScores() {
+        let tbody, tr;
+        let value = 0;
+        let avail = 0;
+        let range = 10;    // Maximum score per quiz
+        let numScores = 5; // Number of scores to be displayed  
+        let yChart = [];   // List of chart ordinates
+
+        setSpin(true);
+        setChartLine();
+        showEl(elNothing, false);
+        showEl(elTmeter, true);
+        showEl(chart, true);
+
+        let scores = JSON.parse(localStorage.scores);
+
+        tbody = createTable(elTable, 'scoresTable', ['#', 'Hits', 'Date & Time']);
+        scores.slice(-numScores).forEach((score, i) => {
+            tr = createEl(tbody, 'tr');
+            createEl(tr, 'td', i + 1);
+            createEl(tr, 'th', score.hits.toString()); // '0'
+            createEl(tr, 'td', score.dateTime);
+            yChart.push(score.hits);
+            value += score.hits;
+            avail++;
+        })
+
+        setChartLine(yChart);
+        setMeter(value, range, numScores, avail);
+
+        setSpin(false);
+    }
+
+    /**
+     * Hide the scoring area
+     */
+    function hideScores() {
+        elTable.innerHTML = '';
+        showEl(elTmeter, false);
+        showEl(chart, false);
+    }
+
+    /**
+     * Saves the score of the completed quiz
+     */
+    function saveScore() {
+        let scores = [];
+        setSpin(true);
+        setInactiveBtn(btnStart, true);
+
+        if(localStorage.scores) scores = JSON.parse(localStorage.scores);
+        
+        scores.push({'hits': hits, 'dateTime': currentDateTime()});
+        localStorage.setItem('scores', JSON.stringify(scores.slice(-100))); 
+        
+        setSpin(false);
+        setInactiveBtn(btnStart, false);
+    }
+
+    /**
+     * Gets the questions for the Quiz
+     */
+    function getQuiz() {
+        // We ask for the numQ minus one, which we will get apart from the JSON
+        let address  = `https://opentdb.com/api.php?amount=${numQ - 1}&type=multiple`;
+
+        // Gets the questions (minus one) from the API
+        let query = async data => {
+            if (data.response_code) throw Error('API #' + data.response_code);
+            questions = data.results;
+            let question = await getQuestionFireBase(); // Get one question from Firebase
+            questions.unshift(question);
+            setInactiveBtn(btnPlay, false);
+        };
+
+        fetchAPI(address, query);
+    }
+
+    /**
+     * Show a new question
+     */
+    function askQuestion() {
+        let cards = fisherYatesShuffle([0, 1, 2, 3]);
+        let question = questions[qIndex - 1];
+
+        elCatego.innerHTML = question.category;
+        elDiffic.innerHTML = question.difficulty;
+        elQuesti.innerHTML = question.question;
+
+        cards.forEach((card, i) => {
+            el(`#opt${i + 1}`).checked = false;
+            el(`#opt${i + 1}`).setAttribute('value', card); 
+            if (card) {
+                el(`label[for=opt${i + 1}]`).innerHTML = question.incorrect_answers[card - 1];
+            } else {
+                el(`label[for=opt${i + 1}]`).innerHTML = question.correct_answer;
+                console.log(decodeHTMLEntities(question.correct_answer)); // Did someone ask for help?;
+            }
+        })
+
+        setProgress(numQ, qIndex -1);
+    }
+
+    /**
+     * Check the form
+     * @param {event} event prevent
+     */
+    function checkAnswer(event) {
+        event.preventDefault(); // Firefox needs you!
+        setInactiveBtn(btnSend, true);
+        
+        if (!el('input[name="opt"]:checked')) {
+            openDialog('Select an option');
+        } else {
+            window.scroll(0, 0);
+
+            if (form.opt.value == 0) hits++;
+
+            if (qIndex == numQ) {
+                end();
+            } else {
+                qIndex++;
+                askQuestion();
+            }
+        }
+    }
+
+}
